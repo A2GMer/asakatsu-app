698ec34c8882138d2df8a492813178886fd3bbc0
A2GMer <52153162+A2GMer@users.noreply.github.com>
2025-01-07 23:25:17 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { Amplify } from 'aws-amplify';
-
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from './aws-exports';
-
 import PostsList from './components/PostsList';
 
-Amplify.configure(awsExports);
 
 export default function App() {
   return (
